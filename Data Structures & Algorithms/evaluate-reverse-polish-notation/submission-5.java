8a780a3c734e606bdd1879561f56b6c40132436b
Boyan Bonev <75538126+bonevboyan@users.noreply.github.com>
2026-07-22 11:23:23 +0300

    Add: evaluate-reverse-polish-notation - submission-5

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-5.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Collections.reverse(Arrays.asList(tokens));
+        Stack<String> stack = new Stack<>();
+        for (String token: tokens) {
+            stack.push(token);
+        }
+
+        Stack<Integer> numbers = new Stack<>();
+        while (!stack.empty()) {
+
+            while (!stack.empty() && !stack.peek().equals("*") && !stack.peek().equals("/")
+             && !stack.peek().equals("-") && !stack.peek().equals("+")) {
+                numbers.push(Integer.parseInt(stack.pop()));
+            }
+
+            Integer firstNum = numbers.pop();
+
+            if (numbers.empty()) return firstNum;
+
+            Integer secondNum = numbers.pop();
+            String operand = stack.pop();
+
+            int result = -1;
+
+            if (operand.equals("*")) {
+                result = firstNum * secondNum;
+            } else if (operand.equals("/")) {
+                result =  secondNum / firstNum;
+            } else if (operand.equals("-")) {
+                result =  - firstNum + secondNum;
+            } else if (operand.equals("+")) {
+                result = firstNum + secondNum;
+            }
+
+            boolean stackEmpty = stack.empty();
+            if (stackEmpty) {
+                return result;
+            }
+
+            stack.push(String.valueOf(result));
+        }
+
+        return 0;
+    }
+}
