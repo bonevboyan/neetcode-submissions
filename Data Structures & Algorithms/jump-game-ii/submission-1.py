0b4e66c7bc24142b0ae9a57d92d267820c40b6e3
Boyan Bonev <75538126+bonevboyan@users.noreply.github.com>
2026-09-21 07:28:23 +0200

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        proc = [-1] * len(nums)
+        farthest = 0
+
+        for idx, num in enumerate(nums):
+            count = num
+
+            if (count + idx) >= len(nums):
+                count = len(nums) - idx - 1
+
+            if count + idx <= farthest:
+                continue
+
+            while count > 0 :
+                if proc[idx + count] == -1:
+                    proc[idx + count] = idx
+                    farthest = idx
+                count = count - 1
+
+
+        index = len(nums) - 1
+        count = 0
+        while index >= 0:
+            index = proc[index]
+            count = count + 1
+
+        return count - 1
+
+            
+        
+        
